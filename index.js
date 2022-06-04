6649d713dda8c53b5bbd2dd88e04b44f79fda494
winston kiprop <winstonekip50@gmail.com>
2022-06-04 19:04:00 +0300

    commit tree fbb6f03a5850032a93c67946d4bafd0fd8a718f2

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Write your code here!const body = document.querySelector('body')
+
+
+const body = document.querySelector('body')
+body.removeChild(document.querySelector('#main'));
+
+const newHeader = document.createElement('h1');
+newHeader.id  = 'victory';
+newHeader.textContent = 'winstone is the champion';
+body.appendChild(newHeader)
